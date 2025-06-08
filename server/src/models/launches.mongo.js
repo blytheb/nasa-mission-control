83dc1b88c8117ec41744759dae0d3acc095b757f
blytheb <builtbyblythe@gmail.com>
2025-06-07 23:15:15 -1000

    upset operation for planets

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -31,3 +31,6 @@ const launchesSchema = new mongoose.Schema({
         default: true,
     }
 })
+
+//connects launchesSchema with "launches" collection
+module.exports = mongoose.model('Launch', launchesSchema);
