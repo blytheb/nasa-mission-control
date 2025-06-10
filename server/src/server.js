126a78d8a40c465069b29bae11adae8d5e315e5f
blytheb <builtbyblythe@gmail.com>
2025-06-10 00:12:17 -1000

    mapping spacex data to database

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const { mongoConnect } = require('./services/mongo.js');
+const { loadPlanetsData } = require('./models/planets.model.js');
+const { loadLaunchData } = require('./models/launches.model.js');
 
-const mongoose = require('mongoose');
 const app = require('./app.js');
 
-const { loadPlanetsData } = require('./models/planets.model.js');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,6 +16,7 @@ const server = http.createServer(app);
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
 
     server.listen(PORT, ()=>{
         console.log(`Listening on port ${PORT}...`);
